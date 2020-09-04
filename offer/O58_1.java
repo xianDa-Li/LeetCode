f3437efe5c7aa242810f7b940d6c70c8809c48ca
toxic-li <1574438603@qq.com>
2020-09-04 18:47:02 +0900

    Update O58_1 problems

--- offer/O58_1.java
+++ offer/O58_1.java
@@ -7,7 +7,7 @@ package com.company.leetcode.offer;
  * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
  * 为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
  */
-public class T58_1 {
+public class O58_1 {
 
 	/*
 		分隔字符串，之后倒叙遍历拼接字符串，遇到空格则continue;
